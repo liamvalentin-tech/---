25be8837834660975abf2e16a0fa23d10ef43e11
liamvalentin-tech <liam.valentin@discoverystudent.org>
2026-04-02 11:54:35 -0400

    4/2/26

---
+++ Assets/People/Liam/Scripts/Shoot.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour
+{
+    public KeyCode Shootbutton = KeyCode.Mouse0;
+    public bool canShoot = true;
+    public bool IsShooting { get; private set; }
+    public GameObject BulletPrefab;
+    public GameObject GunBarrel;
+    public float Cooldown = 0.5f;
+    public Animator recoilAnim;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+        if (canShoot)
+            {
+        IsShooting = canShoot && Input.GetKey(Shootbutton);
+            recoilAnim.SetTrigger("Recoil");
+            GameObject bullet = Instantiate(BulletPrefab, GunBarrel.transform.position, GunBarrel.transform.rotation);
+            canShoot = false;
+            StartCoroutine(DelaySeconds(Cooldown));
+            }
+        }
+    }
+    private IEnumerator DelaySeconds(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        canShoot = true;
+    }
+}
